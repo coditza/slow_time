3b41938701f5f0398cbb28f295d3b5e445f0da68
Petru Marius Balau <petru@hub47.com>
2020-11-26 21:42:08 +0000

    help

--- slow_time.py
+++ slow_time.py
@@ -6,26 +6,31 @@ from multiprocessing import Pool
 from flask import Flask
 
 
-def f(x):
-    return x * x
+if __name__ != "__main__":
+
+    def f(x):
+        return x * x
 
 
-def handle_request(iterations: int, concurent: int):
-    with Pool(concurent) as p:
-        p.map(f, range(0, iterations))
+    def handle_request(iterations: int, concurent: int):
+        with Pool(concurent) as p:
+            p.map(f, range(0, iterations))
 
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
 
-@app.route('/slow_time')
-def hello_world():
-    ts = time.time()
-    iterations = int(os.environ.get('SLOW_TIME_ITERATIONS', 1))
-    concurent = int(os.environ.get('SLOW_TIME_CONCURENT', 2))
-    print(f"Handling request, iterations={iterations}, concurent={concurent}")
-    handle_request(iterations, concurent)
-    return {
-        "gmt-time": datetime.datetime.now().isoformat(),
-        "duration": time.time() - ts
-    }
+    @app.route('/slow_time')
+    def hello_world():
+        ts = time.time()
+        iterations = int(os.environ.get('SLOW_TIME_ITERATIONS', 1))
+        concurent = int(os.environ.get('SLOW_TIME_CONCURENT', 2))
+        print(f"Handling request, iterations={iterations}, concurent={concurent}")
+        handle_request(iterations, concurent)
+        return {
+            "gmt-time": datetime.datetime.now().isoformat(),
+            "duration": time.time() - ts
+        }
+
+else:
+    print("Usage: SLOW_TIME_ITERATIONS=<int> SLOW_TIME_CONCURENT=<int> gunicorn -w <workers> -b 0.0.0.0:<port> slow_time:app")
